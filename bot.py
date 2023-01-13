56c97bf68501a32350c2bf13068fc1268dbaa423
GaMeNu <98153342+GaMeNu@users.noreply.github.com>
2023-01-13 13:52:44 +0200

    v2.0.0 - 0.3.0 TR
    Added RequestBlock() MCCommand and added better documentation for bot_cmds. The GDv2 system is now fully functional

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@
 # imports
 import asyncio
 # Make SURE the file "bot_cmds.py" is in the same directory as this file!
-from bot_cmds import Run
+import bot_cmds
 import discord
 from discord import app_commands
 from dotenv import load_dotenv
@@ -19,6 +19,7 @@ load_dotenv()
 TOKEN = os.getenv('BWU_DISCORD_TOKEN')
 GUILD = int(os.getenv('DISCORD_GUILD'))
 COMM_CHANNEL = int(os.getenv('DF_COMMUNICATION_CHANNEL'))
+OUT_CHANNEL = int(os.getenv('COMMAND_OUTPUT_CHANNEL'))
 JSON_PATH = os.getenv('GD_STORAGE_PATH')
 VERSION = os.getenv('VERSION')
 
@@ -28,10 +29,11 @@ tree = app_commands.CommandTree(bot)
 guild = discord.utils.find(lambda g: g.id == GUILD, bot.guilds)
 comm_channel: discord.TextChannel = bot.get_channel(COMM_CHANNEL)
 
-logger = logging.Logger(name='BWUBot',level=logging.INFO)
+logger = logging.Logger(name='BWUBot', level=logging.INFO)
 logger.setLevel(logging.DEBUG)
 
 temp_store = {}
+
 
 def load_json_to_store():
     global temp_store
@@ -39,12 +41,14 @@ def load_json_to_store():
     try:
         with open(JSON_PATH, 'r') as f:
             temp_store = json.loads(f.read())
-    
+
     except:
-        logger.critical(f'ERROR: Cannot find storage file at path {JSON_PATH}!')
-    
+        logger.critical(
+            f'ERROR: Cannot find storage file at path {JSON_PATH}!')
+
     else:
         logger.info(f'Successfully loaded JSON file from path {JSON_PATH}')
+
 
 async def sync_commands(channel: discord.TextChannel):
 
@@ -56,34 +60,34 @@ async def sync_commands(channel: discord.TextChannel):
     logger.info('Started syncing all messages in communication channel')
     print('Started syncing all messages in communcation channel')
     async for message in messages:
-        content = message.content.replace('\n', '').replace(' ','').split(';')
+        content = message.content.replace('\n', '').replace(' ', '').split(';')
 
         # iterate on every command, make sure is not a comment
         if (not message.content.startswith('#!')):
             for command in content:
                 if command != '' and command[0] != '#':
-                    await Run.run_command(command)
-            
+                    # This one connection point, so much P A I N
+                    await bot_cmds.run_command(command, bot)
+
             await message.delete()
-        
+
         await asyncio.sleep(0.1)
 
 
 # Command to sync all previously sent msgs
-@tree.command(name='sync', description='Manually sync all messages in channel')
-async def sync(intr: discord.Interaction):
+@tree.command(name='gd-sync', description='Manually sync all messages in channel')
+async def gd_sync(intr: discord.Interaction):
     # Make sure this is the correct channel
     if (intr.channel.id != COMM_CHANNEL):
         await intr.response.send_message('Error: Incorrect channel! Please use this command in <#1062285172378185799> instead!')
         return
 
-    await intr.response.defer()
-    await intr.channel.send('#! Begun syncing!', delete_after=3)
+    await intr.response.send_message('#! Begun syncing!', ephemeral=True)
     await sync_commands(intr.channel)
-    await intr.channel.send('#! Done syncing!', delete_after=3)
+    await intr.followup.send('#! Done syncing!', ephemeral=True)
 
 
-@tree.command(name='gd-search',description='Search for a location to get the player that last placed a block in that location')
+@tree.command(name='gd-search', description='Search for a location to get the player that last placed a block in that location')
 async def gd_search(intr: discord.Interaction, x: float, y: float, z: float):
     global temp_store
     loc = f'[{math.floor(x)},{math.floor(y)},{math.floor(z)}]'
@@ -105,7 +109,7 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='the world burn because of me.'), status=discord.Status.dnd)
     await tree.sync(guild=guild)
 
-    #Sync on online
+    # Sync on online
     await comm_channel.send('#! Begun syncing!', delete_after=3)
     await sync_commands(comm_channel)
     await comm_channel.send('#! Done syncing!', delete_after=3)
@@ -122,5 +126,6 @@ load_json_to_store()
 print(f'Welcome, and thank you for using BWUBot version 2!')
 print(f'BWUBot version {VERSION}')
 print(f'Made by GaMeNu')
-logger.warning('WARNING: This is not yet ready as a functional piece! Please do not run this as the Grief Detector!')
+logger.warning(
+    'WARNING: This is not yet ready as a functional piece! Please do not run this as the Grief Detector!')
 bot.run(TOKEN)
--- bot_cmds.py
+++ bot_cmds.py
@@ -1,50 +1,217 @@
+import discord
 from dotenv import load_dotenv
 import json
 import math
 import os
 
 load_dotenv()
-BOTDATA = os.getenv('GD_STORAGE_PATH')
+JSON_PATH = os.getenv('GD_STORAGE_PATH')
+OUTPUT_CHANNEL = int(os.getenv('COMMAND_OUTPUT_CHANNEL'))
+
+# class _GD is concerned with editing, manipulating, and registering blocks to the storage file
+
+
 class _GD:
-    
+    """
+    This static class is used for editing, manipulating, and registering blocks to the JSON storage file
+
+    class variables:
+        temp_store: A dictionary to store items temporarily before dumping to JSON file
+
+    Methods
+    -------
+
+    sort_dict(dct):
+        Utility function that returns a sorted dictionary (by keys) from a given dictionary
+
+    register_block(cmd):
+        Parses a cmd from this template: ``([x,y,z]:"PlayerName")``
+        to ``key = '[x,y,z]'``, ``value = 'PlayerName'``
+        and adds it to `temp_store`
+
+    commit_blocks():
+        Adds the entries from `temp_store` to the JSON
+
+    """
+
+    # Temporary memory for saving blocks until commiting them with CommitBlocks:
     temp_store = {}
 
-    #Add blocks to temporary memory
+    # sorting method to sort JSON before dumping:
     @staticmethod
-    def register_block(cmd: str):
-        #RegisterBlock([num,num,num]:"PlayerName")
-        cmdblck = cmd[1:-1:].split(':')
+    def sorted_dict(dct: dict) -> dict:
+        """
+        Utility function that returns a sorted dictionary (by keys) from a given dictionary
+
+        Parameters:
+            dct: ``dict``
+                dict to sort
+
+        Returns:
+            ``dict``:
+                Sorted dictionary by keys
+        """
+        temp = dct
+        dct = {}
+        for i in sorted(temp):
+            dct[i] = temp[i]
+
+        return dct
+
+    # Add blocks to temporary memory
+    @staticmethod
+    def register_block(cmd: str) -> None:
+        """
+        Parses a command from a template and adds it to temporary memory
+
+        | Parses a cmd from this template: ``([x,y,z]|"PlayerName")``\n
+        | to ``key = '[x,y,z]'``, ``value = 'PlayerName'``\n
+        | and adds it to `temp_store`
+
+        Parameters:
+            cmd: ``str``:
+                string to parse
+        """
+        # Command template:
+        # RegisterBlock([num,num,num]|"PlayerName")
+
+        # Parse command from string (floor values if recieved floats)
+        cmdblck = cmd[1:-1:].split('|')
+
         flooring = cmdblck[0][1:-1:].split(',')
         for i in range(len(flooring)):
             flooring[i] = math.floor(float(flooring[i]))
-        
         cmdblck[0] = f'[{flooring[0]},{flooring[1]},{flooring[2]}]'
-        
+
         cmdblck[1] = cmdblck[1][1:-1:]
+
+        # Resgister new blocks to temp memory
         _GD.temp_store[cmdblck[0]] = cmdblck[1]
 
-    #Commit the blocks to the JSON
+    # Commit the blocks to the JSON
     @staticmethod
-    def commit_blocks():
-        with open(BOTDATA, 'r') as f:
+    def commit_blocks() -> None:
+        """
+        Adds the entries from `temp_store` to the JSON
+        """
+        # load JSON file to program memory
+        with open(JSON_PATH, 'r') as f:
             storage = dict(json.loads(f.read()))
 
+        # add new entries and sort
         storage.update(_GD.temp_store)
+        storage = _GD.sorted_dict(storage)
 
-        with open(BOTDATA, 'w') as f:
+        # dump new JSON back to file
+        with open(JSON_PATH, 'w') as f:
             f.write(json.dumps(storage))
+
+        # reset memory
         _GD.temp_store = {}
 
-#Master class Run provides a way for running all commands from the main file
-class Run:
-#Master function for determining the command
-    async def run_command(cmd: str):
+    @staticmethod
+    def return_json_as_dict() -> dict:
+        """
+        This method returns the storage JSON file as a dict
+        """
+        with open(JSON_PATH, 'r') as f:
+            ret_dct = json.loads(f.read())
 
-        if cmd.startswith('RegisterBlock('):
-            _GD.register_block(cmd[len('RegisterBlock')::])
-        elif cmd.startswith('CommitBlocks()'):
-            _GD.commit_blocks()
+        return ret_dct
+
+
+class _Output:
+    """
+    This static class is used for sending messages and output
+
+    Methods
+    -------
+
+    out(content, client, channel_id?):
+        Sends a message to the default channel, or to a specific channel by ID
+
+    request_block(cmd, client):
+        Sends a message with a requested block and the requester using a template
+    """
+
+    @staticmethod
+    async def out(content: str, client: discord.Client, channel_id: int = None) -> None:
+        """
+        This function sends a message to the default channel, or to a specific channel if an ID is given.
+
+        Parameters
+        ----------
+
+        content: ``str``
+            Message to send
+
+        client: ``discord.Client``
+            A discord bot client to send the message with
+
+        channel_id: ``int | None``
+            A discord channel ID to send the message to.
+            Sends to the `DEFAULT_CHANNEL` if None is given.
+        """
+
+        if channel_id == None:
+            channel = client.get_channel(OUTPUT_CHANNEL)
+        else:
+            channel = client.get_channel(channel_id)
+
+        await channel.send(content=content)
+
+    async def request_block(cmd: str, client: discord.Client):
+        """
+        Sends a message to the `DEFAULT_CHANNEL` with the requested block and a requester.\n
+        Template: ([x,y,z]|"RequesterName")
+
+        Parameters:
+            cmd: ``str``
+                Command to parse
+            client: ``discord.Client``
+                Discord client to send from
+        """
+        cmdblck = cmd[1:-1].split('|')
+
+        flooring = cmdblck[0][1:-1:].split(',')
+        for i in range(len(flooring)):
+            flooring[i] = math.floor(float(flooring[i]))
+        cmdblck[0] = f'[{flooring[0]},{flooring[1]},{flooring[2]}]'
+
+        cmdblck[1] = cmdblck[1][1:-1:]
+
+        storage = _GD.return_json_as_dict()
+
+        out_str: str
+        if not cmdblck[0] in storage.keys():
+            out_str = f'There doesn\'t seem to be any player saved to location **{cmdblck[0]}**.'
+        else:
+            out_str = f'The last player to modify the block at location **{cmdblck[0]}** is **{storage[cmdblck[0]]}**'
+
+        out_str += f'\n(Requested by {cmdblck[1]})'
+
+        await _Output.out(out_str, client)
+
+
+async def run_command(cmd: str, client: discord.Client = None) -> None:
+    """
+    This function interprets the command passed to it and runs the fitting function.
+
+    Parameters:
+        cmd: ``str``
+            A command to parse
+        client: ``discord.Client``
+            A discord bot client for commands that require it
+    """
+
+    if cmd.startswith('RegisterBlock('):
+        _GD.register_block(cmd[len('RegisterBlock')::])
+    elif cmd.startswith('CommitBlocks()'):
+        _GD.commit_blocks()
+    elif cmd.startswith('RequestBlock('):
+        await _Output.request_block(cmd[len('RequestBlock')::], client)
+
 
 if __name__ == '__main__':
-    print('Error: Please do not run the bot_cmds file! Quitting program...')
+    print('\nError: Please do not run the bot_cmds file! Quitting program...\n')
     quit()
